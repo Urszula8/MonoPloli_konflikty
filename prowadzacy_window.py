4b14c4d7c647d692f2ca5c3c055f6ae864e47440
Antosiewicz <157109309+Antosiewicz@users.noreply.github.com>
2025-05-11 20:26:33 +0200

    Dodanie Opsługi Bazy_pytań do Panelu prowadzącego

--- prowadzacy_window.py
+++ prowadzacy_window.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 import menu
+import question_editor
 
 def powrot_przycisk(okno):
     okno.destroy()
@@ -12,12 +13,24 @@ def uruchom_okno_prowadzacy():
     okno.geometry("1920x1080")
     okno.configure(bg="#e2dbd8")
 
-    # Przygotowanie grafiki przycisku
+    # Przygotowanie grafiki przycisku powrotu
     powrot_img = Image.open("powrot.png").resize((210, 70))
     powrot_photo = ImageTk.PhotoImage(powrot_img)
 
     powrot_button = tk.Button(okno, image=powrot_photo, command=lambda: powrot_przycisk(okno), borderwidth=0)
     powrot_button.image = powrot_photo
     powrot_button.place(x=50, y=30)
+
+    # Przycisk Edytuj bazę pytań
+    edytuj_button = tk.Button(
+        okno,
+        text="Edytuj bazę pytań",
+        command=lambda: [okno.destroy(), question_editor.uruchom_edycje()],
+        bg="#660000",
+        fg="white",
+        width=20,
+        height=2
+    )
+    edytuj_button.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
 
     okno.mainloop()
